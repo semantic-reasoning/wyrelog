5f9bc05a24dcd04b58e356c642c98e26f29d2def
Justin Kim <justin.joy.9to5@gmail.com>
2026-08-04 00:02:56 +0900

    test: exercise detached tenant recovery claims

--- wyrelog/daemon/http.h
+++ wyrelog/daemon/http.h
@@ -506,6 +506,10 @@ void wyl_daemon_http_fail_next_tenant_recovery_repair_for_test
     (SoupServer * server);
 gboolean wyl_daemon_http_take_tenant_recovery_repair_failure_for_test
     (SoupServer * server);
+gboolean wyl_daemon_http_detach_tenant_recovery_slot_for_test
+    (SoupServer * server);
+wyrelog_error_t wyl_daemon_http_seal_tenant_recovery_for_test
+    (SoupServer * server, const gchar * tenant, const gchar * request_id);
 void wyl_daemon_http_set_tenant_recovery_install_checkpoint_for_test
     (SoupServer * server, void (*checkpoint) (gpointer data), gpointer data);
 void wyl_daemon_http_set_tenant_recovery_claim_checkpoint_for_test
