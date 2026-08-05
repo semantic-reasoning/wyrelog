028d2b361ff08575563c78e7afb4257bace1527b
Justin Kim <justin.joy.9to5@gmail.com>
2026-08-05 23:15:50 +0900

    daemon: finalize successful self-arm before response

--- wyrelog/daemon/http.c
+++ wyrelog/daemon/http.c
@@ -13252,6 +13252,13 @@ mfa_enroll_confirm_handler (SoupServer *server, SoupServerMessage *msg,
     set_json_error (msg, status, error);
     return;
   }
+  if (write.state == WYL_DAEMON_POLICY_WRITE_ACTIVE) {
+    rc = wyl_daemon_policy_write_finish_result (&write, rc);
+    if (rc != WYRELOG_E_OK) {
+      set_json_error (msg, 500, WYL_DAEMON_ERR_SERVICE_AUTHORITY_FAILED);
+      return;
+    }
+  }
   set_json_ok (msg);
 }
 
