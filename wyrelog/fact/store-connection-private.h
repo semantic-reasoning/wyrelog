39fc21965bf2c208abb49178275ca08e20168215
Justin Kim <justin.joy.9to5@gmail.com>
2026-09-02 06:39:07 +0900

    fact: poison stores after failed transaction cleanup

--- wyrelog/fact/store-connection-private.h
+++ wyrelog/fact/store-connection-private.h
@@ -11,13 +11,36 @@
 #include "wyrelog/error.h"
 
 typedef struct wyl_fact_store_t wyl_fact_store_t;
+typedef struct WylFactStoreConnectionSession WylFactStoreConnectionSession;
 
-typedef struct
+struct WylFactStoreConnectionSession
 {
   wyl_fact_store_t *store;
   duckdb_connection connection;
+  GThread *owner;
   gboolean held;
-} WylFactStoreConnectionSession;
+};
+
+typedef enum
+{
+  WYL_FACT_STORE_TRANSACTION_APPEND_CORE = 0,
+  WYL_FACT_STORE_TRANSACTION_RETRACT_BY_BATCH,
+  WYL_FACT_STORE_TRANSACTION_COMPOUND_PUT,
+  WYL_FACT_STORE_TRANSACTION_FORGET_COMPLETE,
+} WylFactStoreTransactionKind;
+
+typedef enum
+{
+  WYL_FACT_STORE_TRANSACTION_BEFORE_COMMIT = 0,
+  WYL_FACT_STORE_TRANSACTION_BEFORE_ROLLBACK,
+} WylFactStoreTransactionPhase;
+
+typedef struct
+{
+  WylFactStoreConnectionSession *session;
+  WylFactStoreTransactionKind kind;
+  gboolean open;
+} WylFactStoreTransaction;
 
 wyrelog_error_t wyl_fact_store_connection_session_begin
   (wyl_fact_store_t * store, WylFactStoreConnectionSession * session);
@@ -25,3 +48,8 @@ duckdb_connection wyl_fact_store_connection_session_get
   (const WylFactStoreConnectionSession * session);
 void wyl_fact_store_connection_session_end
   (WylFactStoreConnectionSession * session);
+wyrelog_error_t wyl_fact_store_transaction_begin
+  (WylFactStoreConnectionSession * session, WylFactStoreTransactionKind kind,
+    WylFactStoreTransaction * transaction);
+wyrelog_error_t wyl_fact_store_transaction_finish
+  (WylFactStoreTransaction * transaction, wyrelog_error_t primary_rc);
