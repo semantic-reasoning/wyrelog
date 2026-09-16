68ab0a75a5cc356d0681594f16282b4d442cb246
Justin Kim <justin.joy.9to5@gmail.com>
2026-09-16 22:40:48 +0900

    client: expose schema quota status

--- wyrelog/wyl-client.c
+++ wyrelog/wyl-client.c
@@ -1851,6 +1851,102 @@ wyl_client_fact_quota_configure (WylClient *client, const gchar *tenant,
 }
 
 void
+wyl_client_fact_schema_quota_status_clear
+  (WylClientFactSchemaQuotaStatus *status)
+{
+  if (status == NULL)
+    return;
+  g_clear_pointer (&status->tenant_id, g_free);
+  *status = (WylClientFactSchemaQuotaStatus) { 0 };
+}
+
+static wyrelog_error_t
+client_fact_schema_quota_request (WylClient *client, const gchar *tenant,
+    gboolean configure, guint64 hard_limit, gint64 guard_timestamp,
+    const gchar *guard_loc_class, gint64 guard_risk,
+    WylClientFactSchemaQuotaStatus *out_status)
+{
+  if (out_status == NULL || (configure && hard_limit > G_MAXINT64))
+    return WYRELOG_E_INVALID;
+  wyl_client_fact_schema_quota_status_clear (out_status);
+  g_autofree gchar *base_url = NULL;
+  g_autofree gchar *access_token = NULL;
+  g_autofree gchar *session_token = NULL;
+  wyrelog_error_t rc = client_fact_prepare (client, tenant, guard_timestamp,
+          guard_loc_class, guard_risk, &base_url, &access_token,
+          &session_token);
+  if (rc != WYRELOG_E_OK)
+    return rc;
+  g_autofree gchar *guard_query = client_fact_guard_query (tenant,
+          guard_timestamp, guard_loc_class, guard_risk,
+          access_token != NULL && access_token[0] != '\0' ? NULL : session_token);
+  g_autofree gchar *uri = configure
+      ? g_strdup_printf ("%s/facts/quota?%s&dimension=schema_count&limit=%" G_GUINT64_FORMAT,
+          base_url, guard_query, hard_limit)
+      : g_strdup_printf ("%s/facts/quota?%s&dimension=schema_count",
+          base_url, guard_query);
+  g_autoptr (SoupMessage) message = soup_message_new (configure ? "POST" : "GET",
+          uri);
+  if (message == NULL)
+    return WYRELOG_E_INVALID;
+  client_fact_attach_auth (message, access_token);
+  g_autoptr (GBytes) body = NULL;
+  rc = client_send_fact_message (client, message, &body);
+  if (rc != WYRELOG_E_OK) {
+    if (rc == WYRELOG_E_POLICY && client->last_http_status == 409
+        && g_strcmp0 (client->last_error_code,
+        "fact_quota_limit_below_usage") == 0)
+      return WYRELOG_E_CONFLICT;
+    return rc;
+  }
+  gsize size = 0;
+  const gchar *data = g_bytes_get_data (body, &size);
+  g_autofree gchar *response_tenant = parse_simple_json_string_member
+        (data, size, "tenant_id");
+  g_autofree gchar *dimension = parse_simple_json_string_member
+        (data, size, "dimension");
+  guint64 limit = 0, registered = 0;
+  if (g_strcmp0 (response_tenant, tenant) != 0
+      || g_strcmp0 (dimension, "schema_count") != 0
+      || !parse_simple_json_uint64_member (data, size, "registered", &registered))
+    return WYRELOG_E_IO;
+  const gchar *limit_start = g_strstr_len (data, (gssize) size, "\"limit\":");
+  if (limit_start == NULL)
+    return WYRELOG_E_IO;
+  limit_start += strlen ("\"limit\":");
+  if ((gsize) (data + size - limit_start) >= strlen ("null")
+      && strncmp (limit_start, "null", strlen ("null")) == 0)
+    out_status->has_limit = FALSE;
+  else if (!parse_simple_json_uint64_member (data, size, "limit", &limit))
+    return WYRELOG_E_IO;
+  else {
+    out_status->has_limit = TRUE;
+    out_status->hard_limit = limit;
+  }
+  out_status->tenant_id = g_steal_pointer (&response_tenant);
+  out_status->registered = registered;
+  return WYRELOG_E_OK;
+}
+
+wyrelog_error_t
+wyl_client_fact_schema_quota_status (WylClient *client, const gchar *tenant,
+    gint64 guard_timestamp, const gchar *guard_loc_class, gint64 guard_risk,
+    WylClientFactSchemaQuotaStatus *out_status)
+{
+  return client_fact_schema_quota_request (client, tenant, FALSE, 0,
+             guard_timestamp, guard_loc_class, guard_risk, out_status);
+}
+
+wyrelog_error_t
+wyl_client_fact_schema_quota_configure (WylClient *client, const gchar *tenant,
+    guint64 hard_limit, gint64 guard_timestamp, const gchar *guard_loc_class,
+    gint64 guard_risk, WylClientFactSchemaQuotaStatus *out_status)
+{
+  return client_fact_schema_quota_request (client, tenant, TRUE, hard_limit,
+             guard_timestamp, guard_loc_class, guard_risk, out_status);
+}
+
+void
 wyl_client_fact_write_rate_quota_status_clear
   (WylClientFactWriteRateQuotaStatus *status)
 {
